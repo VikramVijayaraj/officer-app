b665f84e110d3de2b76dda9e15ab0e5919d252ff
VikramVijayaraj <vikramvijayaraj31@gmail.com>
2022-05-23 21:20:51 +0530

    Added Title

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
+      <title>Verifier Portal</title>
       <body className="bg-slate-50">
         <Main />
         <NextScript />
